f575855653590d8b28c8862d9d9a3b70ccd90f85
inputTitleHere <seanbryan97@naver.com>
2022-07-11 17:40:20 +0900

    my Servlet

---
+++ src/main/java/com/kh/together/BaikServlet.java
@@ -0,0 +1,25 @@
+package com.kh.together;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class BaikServlet
+ */
+@WebServlet("/baikserlvet")
+public class BaikServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("얍얍얍");
+	}
+
+}
